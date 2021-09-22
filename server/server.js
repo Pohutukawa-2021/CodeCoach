39b06a363377f91c3bca4d11d78bf44dab316208
LouisDVL <louis.lising@outlook.com>
2021-09-22 19:34:12 +1200

    finished connecting socket

--- server/server.js
+++ server/server.js
@@ -1,9 +1,39 @@
-const path = require('path')
-const express = require('express')
+const io = require('socket.io')({
+  cors: {
+    origin: '*',
+  },
+});
 
-const server = express()
+const jwt = require('express-jwt')
+const socketioJwt = require('socketio-jwt');
+const jwks = require('jwks-rsa');
 
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
+const secret = jwks.expressJwtSecret({
+  cache: true,
+  rateLimit: true,
+  jwksRequestsPerMinute: 5,
+  jwksUri: 'https://dev-ngqwdtsq.us.auth0.com/.well-known/jwks.json'
+});
 
-module.exports = server
+io.use(
+  socketioJwt.authorize({
+    secret,
+    handshake: true,
+  })
+)
+
+io.on('connection', (socket) => {
+  console.log('A user has joined', socket.decoded_token);
+  
+  socket.on('action', (action) => {
+    switch (action.type) {
+      case 'server/hello':
+        console.log('hello')
+        socket.emit('action', {type: 'hello'})
+    }
+  })
+})
+
+const port = 3001;
+io.listen(port)
+console.log('Server is listening on port ', port)
