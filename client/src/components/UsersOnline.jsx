08de505ef2066a347a9f683c78640453c5bed320
LouisDVL <louis.lising@outlook.com>
2021-09-23 19:08:16 +1200

    fixed online users

--- client/src/components/UsersOnline.jsx
+++ client/src/components/UsersOnline.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { NavLink } from "react-router-dom"
 
 function UsersOnline() {
   const usersOnline = useSelector((state) => state.usersOnline)
@@ -7,10 +8,13 @@ function UsersOnline() {
   function setUsersOnlineList() {
     let test = []
     Object.entries(usersOnline).forEach(([key, value]) => {
-      test.push(key)
-      console.log(value)
+      test.push(<li key={key} id={value.user.auth_id}>
+        <NavLink to={`/app`}>
+          <img src={value.user.image_url} alt={value.user.username} />
+        </NavLink>
+      </li>)
     })
-    return test.map(key => <li>{key}</li>)
+    return test
   }
 
   console.log(usersOnline)
