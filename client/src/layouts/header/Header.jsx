aeeca962f836db875653f29a8ff46d97e66af9eb
Dianne Plaza <d.plaza29@me.com>
2021-09-23 16:35:54 +1200

    create layout folder to include header and sidebars

---
+++ client/src/layouts/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../../components/navigation/Nav'
+
+function Header() {
+  return (
+    <div className='header-container'>
+      <span>Logo</span>
+      <Nav />
+    </div>
+  )
+}
+
+export default Header
