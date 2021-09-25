e3ccf3b88d31b6a692f4ce1f0206e65a89432ced
LouisDVL <louis.lising@outlook.com>
2021-09-25 18:41:42 +1200

    changed port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const port = process.eventNames.PORT;
+const port = process.env.PORT;
 const {
   getUserData,
   createUser,
