da03168c2324c5cc4235f3e99a8ff64f28a1675e
LouisDVL <louis.lising@outlook.com>
2021-09-27 20:31:40 +1300

    'deploy'

--- client/src/Pages/GreetPage.jsx
+++ client/src/Pages/GreetPage.jsx
@@ -1,7 +1,7 @@
-import React from "react"
-import LoginButton from "../components/buttons/LoginButton"
-import {Redirect} from 'react-router-dom'
-import {useAuth0} from '@auth0/auth0-react'
+import React from "react";
+import LoginButton from "../components/buttons/LoginButton";
+import { Redirect } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function GreetPage() {
   const { isAuthenticated } = useAuth0();
@@ -11,17 +11,25 @@ function GreetPage() {
   }
   return (
     <div>
-      <img src="/logo.png" alt="codecoach-logo" className='logo-image' />
+      <img
+        src="https://github.com/Pohutukawa-2021/CodeCoach/blob/main/client/public/Logo.png"
+        alt="codecoach-logo"
+        className="logo-image"
+      />
       <div className="center">
         <p className="greeting">A new way to connect with developers</p>
-        <img src="/connection.png" alt="connected" className='greetimage' />
+        <img
+          src="https://github.com/Pohutukawa-2021/CodeCoach/blob/main/client/public/Connection.png"
+          alt="connected"
+          className="greetimage"
+        />
         <p className="words">Connect・Learn・Code</p>
-        </div>
-        <div className="center">
-      <LoginButton />
-        </div>
+      </div>
+      <div className="center">
+        <LoginButton />
+      </div>
     </div>
-  )
+  );
 }
 
 export default GreetPage;
