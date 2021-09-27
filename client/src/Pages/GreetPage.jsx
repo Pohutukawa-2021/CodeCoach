b8df1ba2aa16a693a87b697fe80206bba594e879
LouisDVL <louis.lising@outlook.com>
2021-09-28 06:22:45 +1300

    'deploy'

--- client/src/Pages/GreetPage.jsx
+++ client/src/Pages/GreetPage.jsx
@@ -11,18 +11,10 @@ function GreetPage() {
   }
   return (
     <div>
-      <img
-        src="https://github.com/Pohutukawa-2021/CodeCoach/blob/main/client/public/Logo.png"
-        alt="codecoach-logo"
-        className="logo-image"
-      />
+      <img src="/Logo.png" alt="codecoach-logo" className="logo-image" />
       <div className="center">
         <p className="greeting">A new way to connect with developers</p>
-        <img
-          src="https://github.com/Pohutukawa-2021/CodeCoach/blob/main/client/public/Connection.png"
-          alt="connected"
-          className="greetimage"
-        />
+        <img src="/Connection.png" alt="connected" className="greetimage" />
         <p className="words">Connect・Learn・Code</p>
       </div>
       <div className="center">
