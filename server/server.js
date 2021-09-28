fb33e46fd0bda54c7a755d1b0843909eed971202
Dianne Plaza <d.plaza29@me.com>
2021-09-29 11:39:46 +1300

    update updatepost and addvotes function

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const {
   addCommentById,
   getCommentsByPost,
   updatePost,
+  updateVote
 } = require("./db/post");
 
 //message functions
@@ -130,11 +131,21 @@ io.on("connection", (socket) => {
         });
         break;
       case "server/updatePost":
+        console.log(action.data);
         updatePost(action.data).then(() => {
           getAllPosts().then((allPosts) => {
             io.emit("action", { type: "setPosts", data: allPosts });
           });
         });
+        break;
+      case "server/counter":
+        console.log('action', action.data);
+          updateVote(action.data).then(() =>{
+            getAllPosts().then((allPosts) => {
+              io.emit("action", { type: "setPosts", data: allPosts });
+            });
+          })
+        break;
     }
   });
 });
