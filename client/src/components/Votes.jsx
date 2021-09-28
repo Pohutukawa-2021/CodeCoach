721d5bb75a2dab14a73b032e127b3e89daadd51f
Dianne Plaza <d.plaza29@me.com>
2021-09-28 14:40:02 +1300

    update vote-component with button and disable onclick

--- client/src/components/Votes.jsx
+++ client/src/components/Votes.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../redux/actions/counter'
 
 function Votes() {
+  const counter = useSelector((state) => state.counter)
+  const [disable, setDisable] = useState(false)
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <p>up votes</p>
-      <p>down votes</p>
+      <button disabled={disable} onClick={() => {
+        dispatch(increment())
+        setDisable(true)
+        }}>
+          Up Vote
+      </button>
+      <p>{counter}</p>
+      <button disabled={disable} onClick={() => {
+        dispatch(decrement())
+        setDisable(true)
+        }}>
+          Down Vote
+      </button>
     </div>
   )
 }
 
 export default Votes
+
+// disable when one of the buttons is clicked - onclick will setState to true ??
+// can only pick either or ?
+
