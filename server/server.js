c6bd8d998a12972a2e7b7990ccd87167e895702c
louis lising <louis@louiss-MacBook-Pro.local>
2021-11-05 18:10:10 +1300

    added cors

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const cors = require("cors");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(cors());
 app.get("/*", (req, res, next) =>
   res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
